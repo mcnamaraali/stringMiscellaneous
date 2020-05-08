c12c193eb363c63a623790f543190cdf0f58a15b
Ali <learncomputerswithali@gmail.com>
2020-05-08 14:45:39 +0200

    iterating through the string the other way

--- src/StringMiscellaneous.java
+++ src/StringMiscellaneous.java
@@ -9,7 +9,10 @@ public class StringMiscellaneous {
 		
 		System.out.printf("%nstring length is: %d%n", string.length());
 		
-		for(int count = string.length()-1; count >= 0; count--) {
+		for(int count = 0; count <= string.length()-1; count++) { //hello the correct way from 0 to the length
+			/*it is also possible to write the for as for(int count = 0; count < string.length(); count++)
+			remember length is not the same as amount of elements - 
+			this is similar to an array in this case for using .length for String.*/
 			System.out.printf("%ncharAt: %c%n", string.charAt(count));
 		}
 		
